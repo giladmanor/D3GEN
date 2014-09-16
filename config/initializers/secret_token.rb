fdccc5f85fbe170bccd1ea7c01e812d8061a4110
Gilad <giladmanor01@gmail.com>
2014-09-16 09:16:33 +0300

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3GEN::Application.config.secret_key_base = '52d3f2cd2f0023d9c501a597bc888bb886837deb534bd2d7df30e770666be8c45e7be3fb0bd2eebc506ab7d21d4a38e7a30806f5cc7cf72beb23eb4149b90c06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3GEN::Application.config.session_store :cookie_store, key: '_D3GEN_session'
